39f586d57ac437e49e4e79acbd7ebd8018439fb5
Alyssa Wind <alyssawind@pop-os.localdomain>
2021-07-27 11:36:15 -0300

    test 1 not working

--- __tests__/Tests.spec.js
+++ __tests__/Tests.spec.js
@@ -1,3 +1,15 @@
-import Bill from "../Bill"
+import {Bill} from "./Bill"
 import PaymentTicket from "../PaymentTicket"
+import {billProcessor} from "../BillProcessor";
 
+test('Deve retornar a fatura marcada como PAGA, e três pagamentos do tipo BOLETO criados',()=>{
+    const bill = new Bill("name", new Date.now(), 1500)
+    const paymentTickets = Array.of(
+        new PaymentTicket('1', new Date.now(), 500),
+        new PaymentTicket('2', new Date.now(), 400),
+        new PaymentTicket('3', new Date.now(), 600)
+    )
+    billProcessor(bill, paymentTickets)
+    expect(bill.payed).toBe(true)
+    expect(bill.payments).toBe(paymentTickets)
+})
