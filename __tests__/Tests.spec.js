cd2fcffeb465271e7245737434c1f1f93f4ef795
Alyssa Wind <alyssawind@pop-os.localdomain>
2021-07-27 12:01:43 -0300

    test 1 not working 2

--- __tests__/Tests.spec.js
+++ __tests__/Tests.spec.js
@@ -1,15 +1,20 @@
-import {Bill} from "./Bill"
+import {Bill, Payment} from "../Bill"
 import PaymentTicket from "../PaymentTicket"
 import {billProcessor} from "../BillProcessor";
 
 test('Deve retornar a fatura marcada como PAGA, e três pagamentos do tipo BOLETO criados',()=>{
-    const bill = new Bill("name", new Date.now(), 1500)
+    const bill = new Bill("name", Date.now(), 1500)
     const paymentTickets = Array.of(
-        new PaymentTicket('1', new Date.now(), 500),
-        new PaymentTicket('2', new Date.now(), 400),
-        new PaymentTicket('3', new Date.now(), 600)
+        new PaymentTicket('1', Date.UTC(2000, 7, 27), 500),
+        new PaymentTicket('2', Date.UTC(2000, 7, 27), 400),
+        new PaymentTicket('3', Date.UTC(2000, 7, 27), 600)
+    )
+    const paymentArray = Array.of(
+        new Payment(500, Date.UTC(2000, 7, 27), 'BOLETO'),
+        new Payment(400, Date.UTC(2000, 7, 27), 'BOLETO'),
+        new Payment(600, Date.UTC(2000, 7, 27), 'BOLETO')
     )
     billProcessor(bill, paymentTickets)
     expect(bill.payed).toBe(true)
-    expect(bill.payments).toBe(paymentTickets)
+    expect(bill.payments).toEqual(paymentArray)
 })
