bb13b37e58920bc43e5944afc8bbf0f0dd01e370
Alyssa Wind <alyssawind@pop-os.localdomain>
2021-07-27 13:20:11 -0300

    test 3 reafactored

--- BillProcessor.js
+++ BillProcessor.js
@@ -1,14 +1,14 @@
 import {Bill, Payment} from "./Bill"
 import PaymentTicket from "./PaymentTicket"
 
-export function billProcessor(bill, paymentTickets){
+export function billProcessor(bill, paymentTickets, typeOfPayment = 'BOLETO'){
     let value = 0
     bill.payments.forEach((payment) => {
             value = payment.value
         }
     )
     paymentTickets.forEach((payment)=>{
-        bill.payments = bill.payments.concat(new Payment(payment.value, payment.date, 'BOLETO'))
+        bill.payments = bill.payments.concat(new Payment(payment.value, payment.date, typeOfPayment))
         value += payment.value
     })
     if (value >= bill.value) {
