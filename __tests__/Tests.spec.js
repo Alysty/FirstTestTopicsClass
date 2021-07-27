3de9dd26cc3464d8b37038bdc176226e9a7dacba
Alyssa Wind <alyssawind@pop-os.localdomain>
2021-07-27 13:30:53 -0300

    test 4 working

--- __tests__/Tests.spec.js
+++ __tests__/Tests.spec.js
@@ -50,3 +50,23 @@ test('Deve retornar a fatura marcada como nao PAGA, e dois pagamentos do tipo BO
     expect(bill.payed).toBe(false)
     expect(bill.payments).toEqual(paymentArray)
 })
+
+test('Deve retornar a fatura marcada como PAGA, e tres pagamentos do tipo BOLETO criados apos receber uma quantidade maior que a esperada de dinheiro em pagamentos separados',()=>{
+    const bill = new Bill("name", Date.now(), 1500)
+    let paymentTickets = Array.of(
+        new PaymentTicket('1', Date.UTC(2000, 7, 27), 1000),
+        new PaymentTicket('2', Date.UTC(2000, 7, 27), 500)
+    )
+    const paymentArray = Array.of(
+        new Payment(1000, Date.UTC(2000, 7, 27), 'BOLETO'),
+        new Payment(500, Date.UTC(2000, 7, 27), 'BOLETO'),
+        new Payment(600, Date.UTC(2000, 7, 27), 'BOLETO')
+    )
+    billProcessor(bill, paymentTickets)
+    paymentTickets = Array.of(
+        new PaymentTicket('3', Date.UTC(2000, 7, 27), 600)
+    )
+    billProcessor(bill, paymentTickets)
+    expect(bill.payed).toBe(true)
+    expect(bill.payments).toEqual(paymentArray)
+})
