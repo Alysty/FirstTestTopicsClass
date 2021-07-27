f3000c25c8515974d9ca2bae35093067e1f0eeb2
Alyssa Wind <alyssawind@pop-os.localdomain>
2021-07-27 13:14:22 -0300

    Test 2 not working

--- __tests__/Tests.spec.js
+++ __tests__/Tests.spec.js
@@ -2,12 +2,29 @@ import {Bill, Payment} from "../Bill"
 import PaymentTicket from "../PaymentTicket"
 import {billProcessor} from "../BillProcessor";
 
-test('Deve retornar a fatura marcada como PAGA, e três pagamentos do tipo BOLETO criados',()=>{
+test('Deve retornar a fatura marcada como PAGA, e três pagamentos do tipo BOLETO criados apos receber a quantidade extada de dinheiro esperada',()=>{
     const bill = new Bill("name", Date.now(), 1500)
     const paymentTickets = Array.of(
         new PaymentTicket('1', Date.UTC(2000, 7, 27), 500),
         new PaymentTicket('2', Date.UTC(2000, 7, 27), 400),
         new PaymentTicket('3', Date.UTC(2000, 7, 27), 600)
+    )
+    const paymentArray = Array.of(
+        new Payment(500, Date.UTC(2000, 7, 27), 'BOLETO'),
+        new Payment(400, Date.UTC(2000, 7, 27), 'BOLETO'),
+        new Payment(600, Date.UTC(2000, 7, 27), 'BOLETO')
+    )
+    billProcessor(bill, paymentTickets)
+    expect(bill.payed).toBe(true)
+    expect(bill.payments).toEqual(paymentArray)
+})
+
+test('Deve retornar a fatura marcada como PAGA, e três pagamentos do tipo BOLETO criados apos receber uma quantidade maior que a s esperada de dinheiro',()=>{
+    const bill = new Bill("name", Date.now(), 1500)
+    const paymentTickets = Array.of(
+        new PaymentTicket('1', Date.UTC(2000, 7, 27), 1000),
+        new PaymentTicket('2', Date.UTC(2000, 7, 27), 500),
+        new PaymentTicket('3', Date.UTC(2000, 7, 27), 250)
     )
     const paymentArray = Array.of(
         new Payment(500, Date.UTC(2000, 7, 27), 'BOLETO'),
